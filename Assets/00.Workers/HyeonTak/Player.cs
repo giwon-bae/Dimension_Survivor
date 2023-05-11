c9e160aab0f4d19258b98fdf047920734ded7fcc
Danny0203 <shtag0203@gmail.com>
2023-05-11 23:10:50 +0900

    move

---
+++ Assets/00.Workers/HyeonTak/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal"); //x축 이동
+        inputVec.y = Input.GetAxisRaw("Vertical"); //y축 이동
+    }
+    void FixedUpdate()
+    {
+        // 3. 위치 이동
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+}
